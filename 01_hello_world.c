a97c454e828c8b934b0b68241cfebd2411d1a426
Valle <89414923+reijnders@users.noreply.github.com>
2025-02-11 21:17:14 +0100

    Upload, erweitert mit Kommentaren

---
+++ 01_hello_world.c
@@ -0,0 +1,9 @@
+// STD STANDARD, IO INPUT OUTPUT, wird benutzt falls ein input / output gebraucht wird 
+#include <stdio.h>
+// definieren von main function
+int main(void) {
+    //print von gewuenschten text
+    printf("Hello World! :3");
+    //programm ende
+    return 0;
+}
